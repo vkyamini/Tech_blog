fbcf05820bb8415eea93a54db7173df357368ee6
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-05-09 23:07:44 -0700

    added more routes to the handlebars

--- controller/htmlroutes.js
+++ controller/htmlroutes.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const {BlogPost,User,Comment} = require('../models');
 
-router.get("/",async(req,res)=>{
-    res.render("blogs", {
+// router.get("/",(req,res)=>{
+//     res.render("blogs", {
+//         logged_in: req.session.logged_in,
+//     });
+// })
+
+router.get("/",(req,res)=>{
+    BlogPost.findAll({
+        include:[User],
+        include:[Comment]
+    }).then(projData=>{
+        const hbsData = projData.map(proj=>proj.get({plain:true}));
+        console.log(hbsData);
+        res.render("blogs",{
+            allblogpost:hbsData,
+            logged_in: req.session.logged_in
+        })
+    })
+})
+
+
+
+
+
+
+router.get("/login",(req,res)=>{
+    res.render("login", {
         logged_in: req.session.logged_in,
     });
 })
